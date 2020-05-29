31b213aaa695b6b63a020d8fce848f95cbbbc615
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-05-29 06:03:19 +0100

    Add first section examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+fn execute(args: Vec<String>) -> Result<(), ()> {
+    if args.len() != 1 {
+        return Err(());
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), ()> {
+    let args = std::env::args().skip(1).collect();
+    execute(args)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::execute;
+
+    #[test]
+    fn should_error_if_no_arguments_are_given() {
+        let args = Vec::new();
+        let result = execute(args);
+        assert!(result.is_err());
+    }
 }
