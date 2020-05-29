7888866ede15569912115b4391db02c89350d40b
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-05-29 06:19:02 +0100

    Add code to next section
    This is up to 'Some more refactoring'

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-fn execute(args: Vec<String>) -> Result<(), ()> {
+fn execute(args: Vec<String>) -> Result<(), String> {
     if args.len() != 1 {
-        return Err(());
+        return Err("Expected a path to a valid certificate!".to_owned());
     }
     Ok(())
 }
 
-fn main() -> Result<(), ()> {
+fn main() -> Result<(), String> {
     let args = std::env::args().skip(1).collect();
     execute(args)
 }
@@ -16,8 +16,17 @@ mod test {
 
     #[test]
     fn should_error_if_no_arguments_are_given() {
+        // arrange
         let args = Vec::new();
+
+        // act
         let result = execute(args);
+
+        // assert
         assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "Expected a path to a valid certificate!"
+        )
     }
 }
