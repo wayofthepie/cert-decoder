ea3b4c427c5e1155e1da2d22e27dda262008e68d
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-05-30 21:48:05 +0100

    Add work on post rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!")
+fn execute(args: Vec<String>) -> Result<(), String> {
+    if args.len() != 1 {
+        let error = format!(
+            "{}{}",
+            "Error: did not receive a single argument, ",
+            "please invoke cert-decoder as follows: './cert-decoder /path/to/cert'."
+        );
+        return Err(error);
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let args = std::env::args().skip(1).collect();
+    execute(args)
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::execute;
+
+    #[test]
+    fn should_error_if_not_given_a_single_argument() {
+        // arrange
+        let args = Vec::new();
+
+        // act
+        let result = execute(args);
+
+        // assert
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            format!(
+                "{}{}",
+                "Error: did not receive a single argument, ",
+                "please invoke cert-decoder as follows: './cert-decoder /path/to/cert'."
+            )
+        );
+    }
 }
