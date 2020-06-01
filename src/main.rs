19fee59da8d1e4625e2693c1e388be1912e2d9ee
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-06-01 16:10:12 +0100

    Factor out IO

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::path::Path;
 
-fn execute(args: Vec<String>) -> Result<(), String> {
+trait PathValidator {
+    fn exists(&self, path: &str) -> bool;
+}
+
+struct CertValidator;
+
+impl PathValidator for CertValidator {
+    fn exists(&self, path: &str) -> bool {
+        Path::new(path).exists()
+    }
+}
+
+fn execute(validator: impl PathValidator, args: Vec<String>) -> Result<(), String> {
     if args.len() != 1 {
         let error = format!(
             "{}{}",
@@ -9,8 +21,8 @@ fn execute(args: Vec<String>) -> Result<(), String> {
         );
         return Err(error);
     }
-    let path = Path::new(&args[0]);
-    if !path.exists() {
+    let path = &args[0];
+    if !validator.exists(path) {
         return Err(
             "Error: path given as argument does not exist, it must be a path to a certificate!"
                 .to_owned(),
@@ -21,21 +33,33 @@ fn execute(args: Vec<String>) -> Result<(), String> {
 
 fn main() -> Result<(), String> {
     let args = std::env::args().skip(1).collect();
-    execute(args)
+    let validator = CertValidator;
+    execute(validator, args)
 }
 
 #[cfg(test)]
 mod test {
 
-    use crate::execute;
+    use crate::{execute, PathValidator};
+
+    struct FakeValidator {
+        is_path: bool,
+    }
+
+    impl PathValidator for FakeValidator {
+        fn exists(&self, _: &str) -> bool {
+            self.is_path
+        }
+    }
 
     #[test]
     fn should_error_if_not_given_a_single_argument() {
         // arrange
         let args = Vec::new();
+        let validator = FakeValidator { is_path: true };
 
         // act
-        let result = execute(args);
+        let result = execute(validator, args);
 
         // assert
         assert!(result.is_err());
@@ -53,9 +77,10 @@ mod test {
     fn should_error_if_argument_is_not_a_path_which_exists() {
         // arrange
         let args = vec!["does-not-exist".to_owned()];
+        let validator = FakeValidator { is_path: false };
 
         // act
-        let result = execute(args);
+        let result = execute(validator, args);
 
         // assert
         assert!(result.is_err());
