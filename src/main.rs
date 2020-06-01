b5c242bb92eb125117554d2692f5bc8c93082ca3
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-06-01 15:21:26 +0100

    Add path exists check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 fn execute(args: Vec<String>) -> Result<(), String> {
     if args.len() != 1 {
         let error = format!(
@@ -6,6 +8,13 @@ fn execute(args: Vec<String>) -> Result<(), String> {
             "please invoke cert-decoder as follows: ./cert-decoder /path/to/cert."
         );
         return Err(error);
+    }
+    let path = Path::new(&args[0]);
+    if !path.exists() {
+        return Err(
+            "Error: path given as argument does not exist, it must be a path to a certificate!"
+                .to_owned(),
+        );
     }
     Ok(())
 }
@@ -37,6 +46,22 @@ mod test {
                 "Error: did not receive a single argument, ",
                 "please invoke cert-decoder as follows: ./cert-decoder /path/to/cert."
             )
+        );
+    }
+
+    #[test]
+    fn should_error_if_argument_is_not_a_path_which_exists() {
+        // arrange
+        let args = vec!["does-not-exist".to_owned()];
+
+        // act
+        let result = execute(args);
+
+        // assert
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "Error: path given as argument does not exist, it must be a path to a certificate!"
         );
     }
 }
